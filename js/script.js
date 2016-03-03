468c64402ad1701efc4040a3e1c3248032ea02ed
jerseyjack21 <jerseyjack21@gmail.com>
2016-03-03 16:16:56 -0600

    added data.json, first commit as new repo

--- js/script.js
+++ js/script.js
@@ -10,6 +10,8 @@ $(document).ready(function() {
 	// When the page is loaded,
 	// call the loadData() function.
 
+loadData();
+
 });
 
 function loadData() {
@@ -17,6 +19,10 @@ function loadData() {
 	// 2.
 	// Write an AJAX call here to load your data.
 	// Then PASS the data to writeTable();
+
+	$.getJSON("js/donations_over_5k.json", function(data) {
+		writeTable(data);}
+		)
 }
 
 
@@ -29,5 +35,31 @@ function writeTable(data) {
 	// - Contribution Date
 	// - Contributon Information
 	// - Amount
+
+//trying to fix contribution dates to propublica style
+// 	var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+// blog.posts().all(function(posts){
+//   for(var i = 0; i < posts.length; i++){
+//     jQuery('.'+MYWRAPPER_CLASS).append(function(){
+//       var dt = new Date(posts[i].date);
+//       return (posts[i].thumbnail) ? '<div class="coll"><div class="date"><span>'+months[dt.getMonth()]+dt.getDate()+'</span></div><a class="lastpost_title" href="'+posts[i].url+'"><h4>'+posts[i].title+'</h4></a><div class="blog-post"><p>'+posts[i].excerpt+'<p/></div></a><div class="link-read"><a href="'+posts[i].url+'">Read More</a></div>' : '<a href="'+posts[i].url+'"><h4>'+posts[i].title+'</h4></div>';
+
+//     });
+//   }
+// });
+
+
+	$.each(data, function(i, item) {
+
+		if (item["Committee"] === "CITIZENS TO ELECT KURT SCHAEFER ATTORNEY GENERAL") {
+			$("table.donations tbody").append(
+				"<tr>"+
+					"<td class='date'>"+item["Contribution Date"]+"</td>"+
+	                "<td>"+item["Contribution Information"]+"</td>"+
+	                "<td class='amount'>"+item["Amount"]+"</td>"+
+                "</tr>"
+			);
+		}
+	});
 }
 
